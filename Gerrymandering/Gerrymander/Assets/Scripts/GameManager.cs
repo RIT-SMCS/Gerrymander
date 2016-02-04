aec9b29b7847baa261e184627c5dbe3af6c8f171
Sean Maraia <selio@me.com>
2016-02-04 11:38:56 -0500

    Created UIManager script, attached it to the Canvas, and modified the GameManager script to hold this

--- Gerrymandering/Gerrymander/Assets/Scripts/GameManager.cs
+++ Gerrymandering/Gerrymander/Assets/Scripts/GameManager.cs
@@ -6,10 +6,13 @@ using System.Collections;
 //change colors for color-blind people
 enum Affiliation { Red = 0, Blue = 1, Green = 2, };
 public class GameManager : MonoBehaviour {
+    public GameObject uiCanvas;
     List<Node> nodes;
     List<Connector> connectors;
     List<Unit> units;
     int[] partyDistricts = new int[3];
+    UIManager uiManager;
+    
     //Dictionary<Affiliation, int> partyDistricts;
 
     
@@ -17,6 +20,15 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         //partyDistricts[(int)Affiliation.Red]++;	
+        if(uiCanvas != null)
+        {
+            uiManager = uiCanvas.GetComponent<UIManager>();
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            partyDistricts[i] = i;
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,6 +37,25 @@ public class GameManager : MonoBehaviour {
             //after input calculate districts 
             //do not make connectors if there is no valid district made
         //update GUI
+
+        //uiManager.PopText.text = "/" + units.Count;
+
+        foreach(int party in partyDistricts)
+        {
+            switch (party){
+                case (int)Affiliation.Red:
+                    uiManager.GOPText.text = party + "/5 Rep";
+                    break;
+                case (int)Affiliation.Blue:
+                    uiManager.DemText.text = party + "/6 Dem";
+                    break;
+                case (int)Affiliation.Green:
+                    uiManager.IndText.text = party + "/5 Ind";
+                    break;
+                default:
+                    break;
+            }
+        }
         //check for win condition
 	}
 }
---
+++ Gerrymandering/Gerrymander/Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    GameManager gameManager;
+    public GameObject gmObj;
+    public GameObject Goal, Pop, District, GOP, Dem, Ind;
+    public Text GoalText, PopText, DistrictText, GOPText, DemText, IndText;
+	// Use this for initialization
+	void Start () {
+	    if(gmObj != null)
+        {
+            gameManager = gmObj.GetComponent<GameManager>();
+        }
+        GoalText = Goal.GetComponent<Text>();
+        PopText = Pop.GetComponent<Text>();
+        DistrictText = District.GetComponent<Text>();
+        GOPText = GOP.GetComponent<Text>();
+        DemText = Dem.GetComponent<Text>();
+        IndText = Ind.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
