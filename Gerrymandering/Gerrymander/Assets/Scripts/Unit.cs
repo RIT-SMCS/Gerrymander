62aa7b10a0dbb56b5dbfba90ef2878fbd545a18e
Christopher Mercado (RIT Student) <cdm4363@ad.rit.edu>
2016-02-11 14:35:16 -0500

    Added color changing to Units

--- Gerrymandering/Gerrymander/Assets/Scripts/Unit.cs
+++ Gerrymandering/Gerrymander/Assets/Scripts/Unit.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[ExecuteInEditMode]
 public class Unit : MonoBehaviour {
     public Affiliation affiliation;
 	// Use this for initialization
@@ -10,6 +10,17 @@ public class Unit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        switch (affiliation)
+        {
+            case Affiliation.Red:
+                this.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case Affiliation.Green:
+                this.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case Affiliation.Blue:
+                this.GetComponent<Renderer>().material.color = Color.cyan;
+                break;
+        }
 	}
 }
