cf6f8bcc8640f52a60e8e0f3074c682c84ccdb75
SEAN-PC\Sean <selio@me.com>
2019-06-01 12:54:49 -0400

    Level Select buttons added

---
+++ Gerrymandering/Gerrymander/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+    public int levelNumber = 1;
+    public bool completed = false;
+    public Text CompletionText;
+    public Text LevelText;
+
+	// Use this for initialization
+	void Start () {
+        LevelText.text = "" + levelNumber;
+        if (completed)
+        {
+            CompletionText.text = "\uf058";
+            CompletionText.color = new Color(48.0f/255, 171.0f/255, 12.0f/255);
+        }
+        else
+        {
+            CompletionText.text = "\uf057";
+            CompletionText.color = new Color(171.0f /255 , 12.0f/255, 48.0f/255);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Gerrymandering/Gerrymander/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadLatestLevel ()
+    {
+        //TODO: get latest incomplete level scene, and open it.
+    }
+
+    public void LoadLevelSelect()
+    {
+        SceneManager.LoadScene("Lvl_Select", LoadSceneMode.Single);
+    }
+}
